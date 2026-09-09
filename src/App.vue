27b74670b7f36ed6998987cc6d358dbf6e90ce60
Fayman7 <ivan-merz12@mail.ru>
2026-09-09 15:11:31 +0300

    creating a new branch for developing the project. adding new home view template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import counter from './components/counter.vue'
+  import programs from './components/programs.vue';
 </script>
 
 <template>
   <main>
+    <programs />
     <counter />
   </main>
 </template>
---
+++ src/components/programs.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="content">
+
+        <div class="section" id="head">
+            <span id="heading">Harmful habit helper</span>
+        </div>
+
+        <div class="section" id="programs">
+            <div class="card">
+                <span class="description">Курение</span>
+            </div>
+            <div class="card">
+                <span class="description">Алкоголь</span>
+            </div>
+            <div class="card">
+                <span class="description">Лудомания</span>
+            </div>
+        </div>
+
+        <div class="section" id="footer">
+            <img src="../assets/free-icon-down-arrow-2985150.png" alt="scroll down" id="arrow"/>
+            <img src="../assets/free-icon-down-arrow-2985150.png" alt="scroll down" id="arrow"/>
+            <img src="../assets/free-icon-down-arrow-2985150.png" alt="scroll down" id="arrow"/>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+#content {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+}
+.section {
+    padding: 30px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+#heading {
+    font-family: Arial Black, Arial, Helvetica, sans-serif;
+    font-size: 42px;
+}
+#programs {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 5%;
+}
+.card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid black;
+    border-radius: 15px;
+    padding: 50px 10px 50px 10px;
+}
+.description {
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 16px;
+}
+#footer {
+    flex-direction: column;
+}
+#arrow {
+    width: 40px;
+    aspect-ratio: 1/1;
+}
+</style>
